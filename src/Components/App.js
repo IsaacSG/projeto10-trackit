58d72e987583bc262ce317677e65060afd484efa
Isaac Graciano <isaacgraciano123@gmail.com>
2022-05-25 01:42:23 -0300

    Componetes vazios criados

---
+++ src/Components/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./Header";
+import Login from "./Login";
+import Cadastro from "./Cadastro";
+import Habitos from "./Habitos";
+import Hoje from "./Hoje";
+import Historico from "./Historico";
+
+
+export default function App (){
+    return(
+
+        <BrowserRouter>
+            <Routes>
+                <Header />
+                <Route path = "/" element = {<Login />} />
+                <Route path = "/cadastro" element = {<Cadastro />} />
+                <Route path = "/habitos" element = {<Habitos />} />
+                <Route path = "/hoje" element = {<Hoje />} />
+                <Route path = "/historico" element = {<Historico />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
---
+++ src/Components/Login.js
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function Login (){
+    return(
+        <>
+        </>
+    );
+}
